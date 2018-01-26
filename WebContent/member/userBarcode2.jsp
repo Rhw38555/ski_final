5e16eec8db82a70c14de45c14a2397c37dd462d6
rho <nhwoo38555@gmail.com>
2018-01-26 15:52:52 +0900

    새파일추가

---
+++ WebContent/member/userBarcode2.jsp
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<%@ page language="java" contentType="text/xml; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="sql" uri="http://java.sun.com/jsp/jstl/sql" %> 
+
+    
+    
+<sql:query var="rs" dataSource="jdbc/kh">
+	select * from wp_user_barcodes where id =?
+	<sql:param value="${param.value}"/>
+</sql:query>
+
+<members>
+	<code>success</code>
+	<data>
+		{
+			member : [
+				<c:forEach var="row" items="${rs.rows}">
+					{
+						wallet : "${row.wallet}"
+					}
+				</c:forEach>
+			]
+		}
+		
+	</data>
+</members>
---
+++ src/handler/member/MemberUseHistoryPro.java
@@ -0,0 +1,37 @@
+package handler.member;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import barcode.BarcodeDao;
+import barcode.UserBarcodeDataBean;
+import handler.CommandHandler;
+
+
+@Controller
+public class MemberUseHistoryPro implements CommandHandler{
+	
+	@Resource
+	private BarcodeDao barcodeDao;
+	
+	@RequestMapping( "/memberUseHistoryPro" )
+	@Override
+	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		
+		String id = (String)request.getSession().getAttribute("memId");
+		int price = Integer.parseInt(request.getParameter("price"));
+		UserBarcodeDataBean userbarcodeDto = new UserBarcodeDataBean();
+		userbarcodeDto.setId(id);
+		userbarcodeDto.setWallet(price);
+		int result = barcodeDao.chargeUserbarcode(userbarcodeDto);
+		request.setAttribute("result", result);
+		
+		return new ModelAndView( "member/memberUseHistoryPro" );
+	}
+
+}
